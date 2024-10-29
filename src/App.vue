4e9cc5b05829ab0c857fbdbbb4f77c0dc9646ea2
yao_z <971210502@qq.com>
2024-10-29 20:45:08 +0800

    update:更新页面
    以及readme

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import {
-  Document,
+  HomeFilled,
   Menu as IconMenu,
-  Location,
-  Setting,
+  Cpu,
+  Coin,
+  Briefcase,
+  User,
+  ChatRound,
 } from '@element-plus/icons-vue'
 const handleOpen = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
@@ -38,39 +41,76 @@ const { circleUrl, squareUrl, sizeList } = toRefs(state)
         close-on-click-outside="true"
         @open="handleOpen"
         @close="handleClose"
+        style="height: 100vh"
       >
-        <el-affix :offset="20" style="margin-left: 1vw; margin-bottom: 2vh">
-          <el-avatar :size="50" :src="circleUrl" />
-        </el-affix>
+        <el-avatar
+          :size="50"
+          :src="circleUrl"
+          style="margin-left: 2vw; margin-bottom: 5vh"
+        />居中
         <el-sub-menu index="1">
           <template #title>
-            <el-icon><location /></el-icon>
-            <span>第一导航</span>
+            <el-icon><HomeFilled /></el-icon>
+            <span>导航</span>
           </template>
-          <el-menu-item-group title="Group One">
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item two</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu index="1-4">
-            <template #title>item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-          </el-sub-menu>
+          <el-menu-item index="1-1">房间信息</el-menu-item>
+          <el-menu-item index="1-2">房间列表</el-menu-item>
+          <el-menu-item index="1-3" disabled>注册/编辑</el-menu-item>
+          <el-menu-item index="1-4">帮助</el-menu-item>
         </el-sub-menu>
-        <el-menu-item index="2">
-          <el-icon><icon-menu /></el-icon>
-          <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <el-icon><document /></el-icon>
-          <span>Navigator Three</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <el-icon><setting /></el-icon>
-          <span>Navigator Four</span>
-        </el-menu-item>
+        <el-sub-menu index="2">
+          <template #title>
+            <el-icon><icon-menu /></el-icon>
+            <span>板块</span>
+          </template>
+          <el-menu-item index="2-1">搜索</el-menu-item>
+          <el-menu-item index="2-2">动态</el-menu-item>
+          <el-menu-item index="2-3">广场</el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="3">
+          <template #title>
+            <el-icon><Coin /></el-icon>
+            <span>消费</span>
+          </template>
+          <el-menu-item index="3-1">银行</el-menu-item>
+          <el-menu-item index="3-2">炒股</el-menu-item>
+          <el-menu-item index="3-3">骰子</el-menu-item>
+          <el-menu-item index="3-4">商城</el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="4">
+          <template #title>
+            <el-icon><Briefcase /></el-icon>
+            <span>工具</span>
+          </template>
+          <el-menu-item index="4-1">点播</el-menu-item>
+          <el-menu-item index="4-2">便签</el-menu-item>
+          <el-menu-item index="4-3">解析</el-menu-item>
+          <el-menu-item index="4-4">清屏</el-menu-item>
+          <el-menu-item index="4-5">隐式传送</el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="5">
+          <template #title>
+            <el-icon><Cpu /></el-icon>
+            <span>系统</span>
+          </template>
+          <el-menu-item index="5-1">设置</el-menu-item>
+          <el-menu-item index="5-2">关于</el-menu-item>
+          <el-menu-item index="5-3">重载</el-menu-item>
+          <el-menu-item index="5-4">登出</el-menu-item>
+          <el-menu-item index="5-5">2019年4月29日13点23分</el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="6">
+          <template #title>
+            <el-icon><User /></el-icon>
+            <span>联系人</span>
+          </template>
+        </el-sub-menu>
+        <el-sub-menu index="7">
+          <template #title>
+            <el-icon><ChatRound /></el-icon>
+            <span>消息</span>
+          </template>
+        </el-sub-menu>
       </el-menu>
     </el-aside>
     <el-main>
@@ -78,3 +118,4 @@ const { circleUrl, squareUrl, sizeList } = toRefs(state)
     </el-main>
   </el-container>
 </template>
+<style></style>
